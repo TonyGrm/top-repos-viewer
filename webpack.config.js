9240e7d23321559cbd3703f27c7119d7c6de9c1e
tonygrm <tony@grimaldidev.com>
2022-03-12 19:56:18 +0000

    app config

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,24 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude:
-          /node_modules/,
+        exclude: /node_modules/,
         use: {
-          loader:
-            'babel-loader',
+          loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-react',
-              '@babel/preset-env',
-            ],
-            plugins: [
-              '@babel/plugin-transform-runtime',
-            ],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template:
-        './public/index.html',
+      template: './public/index.html',
     }),
   ],
 };
